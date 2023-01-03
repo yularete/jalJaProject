3e08e38a860f98f8e834bac96376d8b53979a52d
yularete <keinikeiwo@gmail.com>
2023-01-03 09:35:55 +0900

    feat: setup jalja project

---
+++ src/main/java/com/jalja/JaljaApplication.java
@@ -0,0 +1,13 @@
+package com.jalja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaljaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaljaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jalja/JaljaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaljaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
