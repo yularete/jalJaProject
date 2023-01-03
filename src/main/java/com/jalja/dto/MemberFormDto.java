cddbfbe457d91e572e48efdbc956ee899c4d6fb8
yularete <keinikeiwo@gmail.com>
2023-01-03 13:45:56 +0900

    feat: 회원 가입 화면으로부터 넘어오는 가입정보를 담을 memberFormDto 생성

---
+++ src/main/java/com/jalja/dto/MemberFormDto.java
@@ -0,0 +1,17 @@
+package com.jalja.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberFormDto {
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String address;
+}
