6baf4903ea8c6c6f0215ff546929c88fe9aa5533
yularete <keinikeiwo@gmail.com>
2023-01-03 20:30:20 +0900

    feat : 페이지 권한 설정하기

--- src/main/java/com/jalja/config/SecurityConfig.java
+++ src/main/java/com/jalja/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,10 +35,13 @@ public class SecurityConfig {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))//로그아웃 URL
                 .logoutSuccessUrl("/"); //로그아웃 성공 시 이동할 URL
 
+        //시큐리티 처리에 HttpServletRequest를 이용한다는 것을 의미.
         http.authorizeRequests()
                 .mvcMatchers("/","/members/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated();
 
+        http.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
         return http.build();
     }
     @Bean
@@ -48,7 +52,14 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+    //인증되지 않은 사용자가 리소스에 접근하였을 때 수행되는 핸들러를 등록
         return authConfiguration.getAuthenticationManager();
     }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        //static 디렉터리 하위 파일들은 인증을 무시하도록 설정
+        return (web) -> web.ignoring().antMatchers("/img/**", "/css/**", "/js/**");
+    }
+
 
 }
