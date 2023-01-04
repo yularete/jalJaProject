b0b82e35ac52df6a6fc86e0f270d7ddbdc57b8cd
yularete <keinikeiwo@gmail.com>
2023-01-04 23:26:53 +0900

    test: member 엔티티 저장 시 자동으로 등록자, 수정자, 등록시간, 수정시간이 저장되는지 테스트

--- src/main/java/com/jalja/entity/BaseEntity.java
+++ src/main/java/com/jalja/entity/BaseEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
 @Getter
-public class BaseEntity {
+public class BaseEntity extends BaseTimeEntity {
 
     @CreatedBy
     @Column(updatable = false)
---
+++ src/test/java/com/jalja/entity/MemberTest.java
@@ -0,0 +1,47 @@
+package com.jalja.entity;
+
+import com.jalja.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class MemberTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @PersistenceContext //EntityManager(영속성 관리)를 빈으로 주입
+    EntityManager em;
+
+    @Test
+    @DisplayName("Auditing 테스트")
+    @WithMockUser(username= "genos", roles = "USER") //genos 라는 사용자가 로그인 한 상태라 가정하고 테스트 진행 가능
+    public void auditingTest(){
+        Member newMember = new Member();
+        memberRepository.save(newMember);
+
+        em.flush();
+        em.clear();
+
+        Member member = memberRepository.findById(newMember.getId()).orElseThrow(EntityNotFoundException::new);
+
+        System.out.println(" register time " + member.getRegTime());
+        System.out.println(" update time " + member.getUpdateTime());
+        System.out.println(" create time " + member.getCreatedBy());
+        System.out.println(" modify time " + member.getModifiedBy());
+
+    }
+}
