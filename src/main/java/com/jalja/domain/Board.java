38b7ec05f11761099f734495c240e5db3ed3e7a5
yularete <keinikeiwo@gmail.com>
2023-01-05 17:22:16 +0900

    feat: @MappedSuperClass를 이용한 공통 속성 처리

--- src/main/java/com/jalja/domain/Board.java
+++ src/main/java/com/jalja/domain/Board.java
@@ -1,20 +1,27 @@
 package com.jalja.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
 
 @Entity
-public class Board {
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bno;
 
+    @Column(length = 500, nullable = false) //칼럼의 길이와 null 허용 여부
     private String title;
 
+    @Column(length = 2000, nullable = false)
     private String content;
 
+    @Column(length = 50, nullable = false)
     private String writer;
 }
