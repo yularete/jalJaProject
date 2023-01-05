afdcfe745a643b835027f6d8bc5d8256ca5283fc
yularete <keinikeiwo@gmail.com>
2023-01-05 19:41:26 +0900

    test : Board Insert 테스트

---
+++ src/test/java/com/jalja/repository/BoardRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.jalja.repository;
+
+import com.jalja.domain.Board;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+@TestPropertySource(locations = "classpath:application-test.properties")
+class BoardRepositoryTest {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void testInsert(){
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            Board board = Board.builder()
+                    .title("title..."+i)
+                    .content("content..."+i)
+                    .writer("user"+(i%10))
+                    .build();
+
+            Board result = boardRepository.save(board);
+            log.info("BNO: " + result.getBno());
+        });
+    }
+}
