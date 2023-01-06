bbeb9400503b7453383f0af70fc012e64ff2a339
yularete <keinikeiwo@gmail.com>
2023-01-06 23:21:50 +0900

    feat : querydsl 이용할 boardSearch Interface

---
+++ src/test/java/com/jalja/repository/search/BoardSearch.java
@@ -0,0 +1,10 @@
+package com.jalja.repository.search;
+
+import com.jalja.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BoardSearch {
+
+    Page<Board> search1(Pageable pageable);
+}
