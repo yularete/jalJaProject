67444729d8a4951ea4c5c3446a0f5e548720479e
yularete <keinikeiwo@gmail.com>
2023-01-07 23:24:12 +0900

    refactoring : boardRepository 선언부에 BS 인터페이스 추가 지정

--- src/main/java/com/jalja/repository/BoardRepository.java
+++ src/main/java/com/jalja/repository/BoardRepository.java
@@ -1,8 +1,13 @@
 package com.jalja.repository;
 
 import com.jalja.domain.Board;
+import com.jalja.repository.search.BoardSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
     //JpaRepository interface를 상속할 때에는 엔티티 타입과 @Id 타입을 지정해 줘야 한다.
+
+    @Query(value = "select now()", nativeQuery = true)
+    String getTime();
 }
