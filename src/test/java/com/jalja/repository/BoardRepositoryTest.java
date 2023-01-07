a9ca736862c91f2077387dfd9a8518a13640d61b
yularete <keinikeiwo@gmail.com>
2023-01-07 23:21:19 +0900

    feat: boardSearch 구현할 클래스 생성

--- src/test/java/com/jalja/repository/BoardRepositoryTest.java
+++ src/test/java/com/jalja/repository/BoardRepositoryTest.java
@@ -6,8 +6,12 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -61,5 +65,22 @@ class BoardRepositoryTest {
     public void testBoardDelete(){
         Long bno = 1L;
         boardRepository.deleteById(bno);
+    }
+
+    @Test
+    public void testBoardPaging(){
+        //1 page order by bno desc
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.findAll(pageable);
+
+        log.info("total count: " + result.getTotalElements());
+        log.info("total pages: " + result.getTotalPages());
+        log.info("page number: " + result.getNumber());
+        log.info("page size: " + result.getSize());
+
+        List<Board> boardList = result.getContent();
+
+        boardList.forEach(board -> log.info(board));
     }
 }
---
+++ src/test/java/com/jalja/repository/search/BoardSearchImpl.java
@@ -0,0 +1,18 @@
+package com.jalja.repository.search;
+
+import com.jalja.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardSearch {
+
+    public BoardSearchImpl(){
+        super(Board.class);
+    }
+
+    @Override
+    public Page<Board> search1(Pageable pageable){
+        return null;
+    }
+}
