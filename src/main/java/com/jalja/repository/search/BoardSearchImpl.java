8b1d6fdf92aaf6c234657e4be6bbec8a41ada6df
yularete <keinikeiwo@gmail.com>
2023-01-09 23:42:36 +0900

    refactoring: dependencies에 'e'하나 빼먹었었고, Querydsl 설정이 제대로 안 돼 Q도메인 클래스가 제대로 안 만들어졌었음! build clean 실행하니까 바로 만들어지더이다 ㄱ-

--- src/main/java/com/jalja/repository/search/BoardSearchImpl.java
+++ src/main/java/com/jalja/repository/search/BoardSearchImpl.java
@@ -1,6 +1,8 @@
 package com.jalja.repository.search;
 
 import com.jalja.domain.Board;
+import com.jalja.domain.QBoard;
+import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
@@ -13,6 +15,13 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
 
     @Override
     public Page<Board> search1(Pageable pageable){
+
+        QBoard board = QBoard.board; //Q도메인 객체
+
+        JPQLQuery<Board> query = from(board); // select.. from board
+
+        query.where(board.title.contains("1")); // where title like...
+
         return null;
     }
 }
